552d0aecc8d8635f21e856fdf7b512ac2654e46f
Jens Andersen <jensandersen@hey.com>
2022-06-22 14:25:18 +0200

    Add a fun loading bar indicating the app is working when changing url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import Layout from '../components/Layout';
-import '../styles/globals.css';
+// import '../styles/globals.css'; TODO: remove if i don't bother adding custom styles
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
