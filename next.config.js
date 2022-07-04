4558680777d281c549fb4d273662d36788cb72eb
Jens Andersen <jensandersen@hey.com>
2022-07-04 17:37:19 +0200

    Styled components global styles, and stuff flickering

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@
 const { withKeystone } = require('@keystone-6/core/next');
 module.exports = withKeystone({
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // https://styled-components.com/docs/advanced#nextjs
+  },
 });
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,20 +7,39 @@ import Document, {
   NextScript,
 } from 'next/document';
 import { ReactElement } from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
+    // See the link for how to serverside render styled-components css.
+    // The link has links to repos with example code, where this code is copied from
+    // https://styled-components.com/docs/advanced#nextjs
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    return initialProps;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render(): ReactElement {
     return (
-      <Html lang='en'>
-        <Head></Head>
+      <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
