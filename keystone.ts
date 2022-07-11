1a2ad4d8198d559cdffc018f8521493cb5ac750a
Jens Andersen <jensandersen@hey.com>
2022-07-11 12:54:04 +0200

    some keystone configuration

--- keystone.ts
+++ keystone.ts
@@ -1,8 +1,14 @@
 // keystone.ts
 
+import { Lists } from '.keystone/types';
 import { config, list } from '@keystone-6/core';
 import { text } from '@keystone-6/core/fields';
-import { Lists } from '.keystone/types';
+import 'dotenv/config';
+
+const sessionConfig = {
+  maxAge: 1000 * 60 * 60 * 24 * 360, // How long they are signed in (1 year)
+  secret: process.env.COOKIE_SECRET, // Secret used to sign the session ID cookie
+};
 
 const Post: Lists.Post = list({
   fields: {
@@ -14,9 +20,14 @@ const Post: Lists.Post = list({
 
 export default config({
   db: { provider: 'sqlite', url: 'file:./app.db' },
+  // cors: { origin: [process.env.FRONTEND_URL], credentials: true },
   experimental: {
     generateNextGraphqlAPI: true,
     generateNodeAPI: true,
   },
   lists: { Post },
+  // ui: {
+  //   isAccessAllowed: () => true,
+  // },
+  // session: sessionConfig,
 });
