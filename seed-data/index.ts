50d4c8f1485356208e60c5faf2a5f691b3cb7fbf
Jens Andersen <jensandersen@hey.com>
2022-07-14 14:11:53 +0200

    Only seed if data is not already existing

--- seed-data/index.ts
+++ seed-data/index.ts
@@ -35,22 +35,37 @@ import { products } from './data';
 //github.com/keystonejs/keystone/blob/6cadee447cd0737a68122dddfa909589cb4c8f4e/examples-staging/ecommerce/seed-data/index.ts
 export async function insertSeedData({ prisma }: KeystoneContext) {
 	console.log(`🌱 Inserting Seed Data: ${products.length} Products`);
+	// console.log(prisma);
 
 	for (const product of products) {
 		console.log(`  🛍️ Adding Product: ${product.name}`);
 
-		const { id } = await prisma.productImage.create({
-			data: {
-				image: JSON.stringify(product.photo),
+		const existingPhoto = await prisma.productImage.findMany({
+			where: {
 				altText: product.description,
 			},
 		});
-		// @ts-ignore
-		delete product.photo;
-		// @ts-ignore
-		// product.photoId = id;
-		product.photo = { connect: { id } };
-		await prisma.product.create({ data: product });
+
+		const existingProduct = await prisma.product.findFirst({
+			where: {
+				name: product.name,
+			},
+		});
+
+		if (!existingPhoto || !existingProduct) {
+			const { id } = await prisma.productImage.create({
+				data: {
+					image: JSON.stringify(product.photo),
+					altText: product.description,
+				},
+			});
+			// @ts-ignore
+			delete product.photo;
+			// @ts-ignore
+			// product.photoId = id;
+			product.photo = { connect: { id } };
+			await prisma.product.create({ data: product });
+		}
 	}
 	console.log(`✅ Seed Data Inserted: ${products.length} Products`);
 	console.log(
