ff87cd9729fe50b8e1f518f4d615406a20349b3a
Jens Andersen <jensandersen@hey.com>
2022-07-16 09:59:59 +0200

    fix typo

--- seed-data/data.ts
+++ seed-data/data.ts
@@ -1,9 +1,9 @@
-function timestamp() {
-	// sometime in the last 30 days
-	const stampy =
-		Date.now() - Math.floor(Math.random() * 1000 * 60 * 60 * 24 * 30);
-	return new Date(stampy).toISOString();
-}
+// function timestamp() {
+// 	// sometime in the last 30 days
+// 	const stampy =
+// 		Date.now() - Math.floor(Math.random() * 1000 * 60 * 60 * 24 * 30);
+// 	return new Date(stampy).toISOString();
+// }
 
 export const products = [
 	{
