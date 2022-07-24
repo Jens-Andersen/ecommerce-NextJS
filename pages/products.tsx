6249bb8e6a2120fd96abd79955641429418f404a
Jens Andersen <jensandersen@hey.com>
2022-07-24 11:10:17 +0200

    remove unused styles

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next';
 import Products from '../components/Products';
-import styles from '../styles/Home.module.css';
+
 
 const ProductsPage: NextPage = () => {
-	return (
-		<div className={styles.container}>
-			<Products />
-		</div>
-	);
+	return <Products />;
 };
 
 export default ProductsPage;
