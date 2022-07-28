288d4e11d4390c2768449b24683e0b4739b45499
Jens Andersen <jensandersen@hey.com>
2022-07-28 10:57:15 +0200

    Stretch header to make it bigger

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,19 +18,19 @@ const LogoStyles = styled.h1`
 `;
 
 const HeaderStyles = styled.header`
-  .bar {
-    border-bottom: 10px solid var(--black, black);
-    display: grid;
-    grid-template-columns: auto 1fr;
-    justify-content: space-between;
-    align-items: center;
-  }
+	.bar {
+		border-bottom: 10px solid var(--black, black);
+		display: grid;
+		grid-template-columns: auto 1fr;
+		justify-content: space-between;
+		align-items: stretch;
+	}
 
-  .sub-bar {
-    display: grid;
-    grid-template-columns: 1fr auto;
-    border-bottom: 1px solid var(--black, black);
-  }
+	.sub-bar {
+		display: grid;
+		grid-template-columns: 1fr auto;
+		border-bottom: 1px solid var(--black, black);
+	}
 `;
 
 function Header(): JSX.Element {
@@ -40,11 +40,11 @@ function Header(): JSX.Element {
 				<LogoStyles>
 					<Link href="/">Hipster Coffee</Link>
 				</LogoStyles>
+				<Nav />
 			</div>
 			<div className="sub-bar">
 				<p>Search</p>
 			</div>
-			<Nav />
 		</HeaderStyles>
 	);
 }
