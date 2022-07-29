8490f996475ab166e94261cadc2de111323344f9
Jens Andersen <jensandersen@hey.com>
2022-07-29 13:52:00 +0200

    custom form hook

--- lib/useForm.ts
+++ lib/useForm.ts
@@ -1,15 +1,40 @@
 import { useState } from 'react';
 
+type ProductListing = {
+	name: string;
+	price: number;
+	description: string;
+};
 export function useForm(initialState = {}) {
-	const [inputs, setInputs] = useState(initialState);
+	const [inputs, setInputs] = useState(initialState as ProductListing);
 
 	const handleInputChange = (e) => {
+		let { name, value, type } = e.target;
+
+		if (type === 'number') value = parseInt(value);
+		if (type === 'file') value[0] = e.target.files;
+
 		setInputs({
 			// copy existing inputs
 			...inputs,
-			[e.target.name]: e.target.value,
+			[name]: value,
 		});
 	};
 
-	return { inputs, handleInputChange };
+	function clearForm() {
+		const blankState = Object.fromEntries(
+			// eslint-disable-next-line no-unused-vars
+			Object.entries(inputs).map(([key, value]) => [key, ''])
+		);
+
+		//@ts-ignore
+		setInputs(blankState);
+	}
+
+	function resetForm() {
+		// @ts-ignore
+		setInputs(initialState);
+	}
+
+	return { inputs, handleInputChange, clearForm, resetForm };
 }
